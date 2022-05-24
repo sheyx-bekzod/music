c3f19ee95b9b960da11846007721cd82d114d983
otabekuz <boltaboyevotabek207@gmail.com>
2022-05-24 14:02:26 +0500

    oatbek

--- static/base.js
+++ static/base.js
@@ -8,8 +8,9 @@ console.log(registerlink);
 const block2 = document.querySelector('.block_2')
 const block1 = document.querySelector('.block_1')
 
-
-login.addEventListener("click",openModal)
+if (login) {
+  login.addEventListener("click", openModal)
+}
 
 function openModal() {
   setTimeout(() => {
@@ -18,24 +19,61 @@ function openModal() {
   }, 1000);
 }
 
-window.addEventListener("click",closeMenu)
-
-function closeMenu(event) {
-    if (event.target === registerWind) {
-      registerWind.classList.add('register-none')
-      registerWind.style.transition = 'easy-in 2.5s'
-    }
+if (window) {
+  window.addEventListener("click", closeMenu)
 }
 
-loginLink.addEventListener("click",openLogin)
-registerlink.addEventListener("click",openRegister)
+function closeMenu(event) {
+  if (event.target === registerWind) {
+    registerWind.classList.add('register-none')
+    registerWind.style.transition = 'easy-in 2.5s'
+  }
+}
 
-function openLogin(){
+if (loginLink && registerlink) {
+  loginLink.addEventListener("click", openLogin)
+  registerlink.addEventListener("click", openRegister)
+}
+
+
+function openLogin() {
   block1.classList.add("block_hide")
   block2.classList.remove('block_hide')
 }
 
-function openRegister(){
+function openRegister() {
   block1.classList.remove("block_hide")
   block2.classList.add('block_hide')
 }
+
+
+// singer_info.html // corousel 
+const wrapper = document.querySelector('.wrapper'),
+  leftBtn = document.querySelector('.leftBtn'),
+  rightBtn = document.querySelector('.rightBtn'),
+  wrapper_album = document.querySelectorAll('.wrapper_img');
+
+let index = 0;
+
+function changeImage() {
+  if (index > wrapper_album.length - 1) {
+    index = 0
+  }
+  else if (index < 0) {
+    index = wrapper_album.length - 1;
+  }
+  wrapper.style.transform = `translateX(${-index * 100}px)`
+}
+
+rightBtn.addEventListener('click', function change() {
+  index++;
+  changeImage()
+});
+
+leftBtn.addEventListener('click', function change() {
+  index--;
+  changeImage()
+});
+
+
+
