c9e0167399479843a8106a776d4240276b32e99c
sheyx-bekzod <abduraxmanovoybek28@gmial.com>
2022-05-28 11:05:33 +0500

    change

--- static/base.js
+++ static/base.js
@@ -40,12 +40,6 @@ window.addEventListener('load', () => {
 
 
   // singer_info.html // corousel 
-  const wrapper = document.querySelector('.wrapper'),
-    leftBtn = document.querySelector('.leftBtn'),
-    rightBtn = document.querySelector('.rightBtn'),
-    wrapper_album = document.querySelectorAll('.wrapper_hover_box');
-
-  let index = 0;
 
   function changeImage() {
     if (index > wrapper_album.length - 2) {
@@ -101,226 +95,225 @@ window.addEventListener('load', () => {
         item.style.transform = `translateX(${-index * 205}px)`
       });
     }
-  }
-
-  if (rightBtn) {
-    rightBtn.addEventListener('click', function change() {
-      index++;
-      changeImage()
-    });
-  }
-
-  if (leftBtn) {
-    leftBtn.addEventListener('click', function change() {
-      index--;
-      changeImage()
-    });
-  }
-
-  const typeMusic = document.querySelectorAll(".song_type h2"),
-    activeIcon = document.querySelectorAll('.song_type i'),
-    popularMusicList = document.querySelectorAll('.popular_music_list')
-
-  typeMusic.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      openInfo(index)
-    })
-  })
-
-  typeMusic.forEach((item, index) => {
-    item.addEventListener("mouseenter", () => {
-      activeHeader(index)
-    })
-  })
 
 
-  function activeHeader(index) {
-    if (popularMusicList[index].style.display !== "none") {
-      typeMusic[index].style.cursor = 'pointer'
+    if (rightBtn) {
+      rightBtn.addEventListener('click', function change() {
+        index++;
+        changeImage()
+      });
     }
-  }
 
-  activeIcon.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      closeInfo(index)
-    })
-  })
+    if (leftBtn) {
+      leftBtn.addEventListener('click', function change() {
+        index--;
+        changeImage()
+      });
+    }
 
+    const typeMusic = document.querySelectorAll(".song_type h2"),
+      activeIcon = document.querySelectorAll('.song_type i'),
+      popularMusicList = document.querySelectorAll('.popular_music_list')
 
-  function closeInfo(index) {
-    activeIcon[index].classList.remove("fa-angle-down")
-    popularMusicList[index].style.display = "none"
-  }
-
-  function openInfo(index) {
-    activeIcon[index].classList.add("fa-angle-down")
-    popularMusicList[index].style.display = "block"
-  }
-
-  // MUSIC
-  const musics = []
-
-  const playBtn = document.querySelectorAll(".play")
-  const audio = document.querySelectorAll(".user_music_box audio")
-  const mainMusicBar = document.querySelector('.main-music')
-  const musicImg = document.querySelectorAll(".user_music_box img")
-  const mp3Name = document.querySelectorAll(".mp3_name p")
-  const mp3Owner = document.querySelectorAll(".mp3_name a")
-  const mp3Ct = document.querySelector(".mp3_ct img")
-  const mp3CtaAbout = document.querySelectorAll('.mp3_ct_about')
-
-  console.log(mp3CtaAbout);
-
-
-  let musicStatus = ""
-
-  audio.forEach(element => {
-    musics.push(element.src)
-  });
-  console.log(playBtn);
-  const pauseMusicbtn = document.querySelector("#pause")
-
-
-  function playAudio() {
-    playBtn.forEach((item, index) => {
+    typeMusic.forEach((item, index) => {
       item.addEventListener("click", () => {
-        playIcon()
-        if (mainMusicBar.src !== musics[index]) {
-          musicStatus = musics[index]
-          mainMusicBar.src = musicStatus
-        } if(mainMusicBar.paused) {
+        openInfo(index)
+      })
+    })
+
+    typeMusic.forEach((item, index) => {
+      item.addEventListener("mouseenter", () => {
+        activeHeader(index)
+      })
+    })
+
+
+    function activeHeader(index) {
+      if (popularMusicList[index].style.display !== "none") {
+        typeMusic[index].style.cursor = 'pointer'
+      }
+    }
+
+    activeIcon.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        closeInfo(index)
+      })
+    })
+
+
+    function closeInfo(index) {
+      activeIcon[index].classList.remove("fa-angle-down")
+      popularMusicList[index].style.display = "none"
+    }
+
+    function openInfo(index) {
+      activeIcon[index].classList.add("fa-angle-down")
+      popularMusicList[index].style.display = "block"
+    }
+
+    // MUSIC
+    const musics = []
+
+    const playBtn = document.querySelectorAll(".play")
+    const audio = document.querySelectorAll(".user_music_box audio")
+    const mainMusicBar = document.querySelector('.main-music')
+    const musicImg = document.querySelectorAll(".user_music_box img")
+    const mp3Name = document.querySelectorAll(".mp3_name p")
+    const mp3Owner = document.querySelectorAll(".mp3_name a")
+    const mp3Ct = document.querySelector(".mp3_ct img")
+    const mp3CtaAbout = document.querySelectorAll('.mp3_ct_about')
+
+    console.log(mp3CtaAbout);
+
+
+    let musicStatus = ""
+
+    audio.forEach(element => {
+      musics.push(element.src)
+    });
+    console.log(playBtn);
+    const pauseMusicbtn = document.querySelector("#pause")
+
+
+    function playAudio() {
+      playBtn.forEach((item, index) => {
+        item.addEventListener("click", () => {
+          playIcon()
+          if (mainMusicBar.src !== musics[index]) {
+            musicStatus = musics[index]
+            mainMusicBar.src = musicStatus
+          } if (mainMusicBar.paused) {
+            pauseMusicbtn.classList.remove('fa-circle-play')
+            pauseMusicbtn.classList.add('fa-circle-pause')
+            item.classList.add("fa-circle-pause")
+            item.classList.remove("fa-circle-play")
+
+            setMusicPage(index)
+            mainMusicBar.play(musics[index])
+          } else {
+            pauseMusicbtn.classList.add('fa-circle-play')
+            pauseMusicbtn.classList.remove('fa-circle-pause')
+            mainMusicBar.pause()
+          }
+        })
+      })
+    }
+
+    playAudio()
+
+    function setMusicPage(index) {
+      const img = musicImg[index].src
+      const owner = mp3Owner[index]
+      const name = mp3Name[index]
+
+      mp3Ct.src = img
+      mp3CtaAbout[0].innerHTML = owner.textContent
+      mp3CtaAbout[1].innerHTML = name.textContent
+    }
+
+    function playIcon() {
+      playBtn.forEach(item => {
+        item.classList.remove('fa-circle-pause')
+        item.classList.add('fa-circle-play')
+      })
+    }
+
+    const progressContainer = document.querySelector(".progress_container"),
+      progress = document.querySelector(".progres"),
+      prevBtn = document.querySelector("#prev"),
+      nextBtn = document.querySelector("#next");
+    console.log(mainMusicBar.src);
+
+    pauseMusicbtn.addEventListener("click", () => {
+      playIcon()
+      if (!status) {
+        if (mainMusicBar.paused) {
+          mainMusicBar.src = musics[0]
           pauseMusicbtn.classList.remove('fa-circle-play')
           pauseMusicbtn.classList.add('fa-circle-pause')
-          item.classList.add("fa-circle-pause")
-          item.classList.remove("fa-circle-play")
-
-          setMusicPage(index)
-          mainMusicBar.play(musics[index])
+          playBtn[0].classList.add('fa-circle-play')
+          mainMusicBar.play()
         } else {
           pauseMusicbtn.classList.add('fa-circle-play')
           pauseMusicbtn.classList.remove('fa-circle-pause')
           mainMusicBar.pause()
         }
-      })
-    })
-  }
-
-  playAudio()
-
-  function setMusicPage(index) {
-    const img = musicImg[index].src
-    const owner = mp3Owner[index]
-    const name = mp3Name[index]
-
-    mp3Ct.src = img
-    mp3CtaAbout[0].innerHTML = owner.textContent
-    mp3CtaAbout[1].innerHTML = name.textContent
-  }
-
-  function playIcon() {
-    playBtn.forEach(item => {
-      item.classList.remove('fa-circle-pause')
-      item.classList.add('fa-circle-play')
-    })
-  }
-
-  const progressContainer = document.querySelector(".progress_container"),
-    progress = document.querySelector(".progres"),
-    prevBtn = document.querySelector("#prev"),
-    nextBtn = document.querySelector("#next");
-  console.log(mainMusicBar.src);
-
-  pauseMusicbtn.addEventListener("click", () => {
-    playIcon()
-    if (!status) {
-      if (mainMusicBar.paused) {
-        mainMusicBar.src = musics[0]
-        pauseMusicbtn.classList.remove('fa-circle-play')
-        pauseMusicbtn.classList.add('fa-circle-pause')
-        mainMusicBar.play()
       } else {
-        pauseMusicbtn.classList.add('fa-circle-play')
-        pauseMusicbtn.classList.remove('fa-circle-pause')
-        mainMusicBar.pause()
-      }
-    }else{
-      if (mainMusicBar.paused) {
+        if (mainMusicBar.paused) {
           pauseMusicbtn.classList.remove('fa-circle-play')
           pauseMusicbtn.classList.add('fa-circle-pause')
           pauseIcon()
           mainMusicBar.play()
-      }else{
-        pauseMusicbtn.classList.add('fa-circle-play')
-        pauseMusicbtn.classList.remove('fa-circle-pause')
-        mainMusicBar.pause()
+        } else {
+          pauseMusicbtn.classList.add('fa-circle-play')
+          pauseMusicbtn.classList.remove('fa-circle-pause')
+          mainMusicBar.pause()
+        }
       }
-    }
-  })
-  function pauseIcon(){
-    playBtn.forEach(item => {
-      item.classList.add("fa-circle-pause")
-      item.classList.remove("fa-circle-play")
     })
-  }
-
-  
-  let status = 0
-
-  prevBtn.addEventListener("click", prevBtnClick)
-  nextBtn.addEventListener('click', nextBtnClick)
-
-  function nextBtnClick() {
-    status++
-    if (status > musics.length - 1) {
-      status = 0
-    } else if (status < 0) {
-      status = musics.length - 1
+    function pauseIcon() {
+      playBtn.forEach(item => {
+        item.classList.add("fa-circle-pause")
+        item.classList.remove("fa-circle-play")
+      })
     }
-    pauseMusic()
-    pauseMusicbtn.classList.remove('fa-circle-play')
-    pauseMusicbtn.classList.add('fa-circle-pause')
-    mainMusicBar.src = musics[status]
-    mainMusicBar.play()
-  }
 
-  function prevBtnClick() {
-    status--
-    if (status > musics.length - 1) {
-      status = 0
-    } else if (status < 0) {
-      status = musics.length - 1
+
+    let status = 0
+
+    prevBtn.addEventListener("click", prevBtnClick)
+    nextBtn.addEventListener('click', nextBtnClick)
+
+    function nextBtnClick() {
+      status++
+      if (status > musics.length - 1) {
+        status = 0
+      } else if (status < 0) {
+        status = musics.length - 1
+      }
+      pauseMusic()
+      pauseMusicbtn.classList.remove('fa-circle-play')
+      pauseMusicbtn.classList.add('fa-circle-pause')
+      mainMusicBar.src = musics[status]
+      mainMusicBar.play()
     }
-    pauseMusic()
-    pauseMusicbtn.classList.remove('fa-circle-play')
-    pauseMusicbtn.classList.add('fa-circle-pause')
-    mainMusicBar.src = musics[status]
-    mainMusicBar.play()
-  }
 
-  function pauseMusic() {
-    mainMusicBar.pause()
-  }
+    function prevBtnClick() {
+      status--
+      if (status > musics.length - 1) {
+        status = 0
+      } else if (status < 0) {
+        status = musics.length - 1
+      }
+      pauseMusic()
+      pauseMusicbtn.classList.remove('fa-circle-play')
+      pauseMusicbtn.classList.add('fa-circle-pause')
+      mainMusicBar.src = musics[status]
+      mainMusicBar.play()
+    }
 
-  function upDateProgress(e) {
-    const { duration, currentTime } = e.srcElement;
-    const percent = (currentTime / duration) * 100;
+    function pauseMusic() {
+      mainMusicBar.pause()
+    }
 
-    progress.style.width = `${percent}%`;
-    progress.style.height = '5px'
-    progress.style.background = 'red'
-  }
+    function upDateProgress(e) {
+      const { duration, currentTime } = e.srcElement;
+      const percent = (currentTime / duration) * 100;
 
-  function setProgress(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const duration = mainMusicBar.duration
-    mainMusicBar.currentTime = (clickX / width) * duration
-  }
+      progress.style.width = `${percent}%`;
+      progress.style.height = '5px'
+      progress.style.background = 'red'
+    }
 
-  mainMusicBar.addEventListener('timeupdate', upDateProgress)
-  progressContainer.addEventListener("click", setProgress)
+    function setProgress(e) {
+      const width = this.clientWidth;
+      const clickX = e.offsetX;
+      const duration = mainMusicBar.duration
+      mainMusicBar.currentTime = (clickX / width) * duration
+    }
 
-})
+    mainMusicBar.addEventListener('timeupdate', upDateProgress)
+    progressContainer.addEventListener("click", setProgress)
 
-
+  })
 
