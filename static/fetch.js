48cd19989910b7bf7cb31a0ae54a6be0c21f8d55
sheyx-bekzod <abduraxmanovoybek28@gmial.com>
2022-05-31 13:00:04 +0500

    change

--- static/fetch.js
+++ static/fetch.js
@@ -3,14 +3,43 @@ const name = document.querySelector('#reg_name'),
     reg_email = document.querySelector('#reg_email'),
     reg_password = document.querySelector('#reg_password'),
     reg_confirm_password = document.querySelector('#reg_confirm_password'),
-    sumbitBtn = document.querySelector("#submit");
+    nameInfo = document.querySelector('.name-info'),
+    emailInfo = document.querySelector('.email-info')
+sumbitBtn = document.querySelector("#submit");
 console.log(sumbitBtn)
 
 
 const elem = document.querySelector('.msg')
 console.log(elem)
 
-reg_password.addEventListener("change", () => {
+
+name.addEventListener("change", () => {
+    if (name.value.length < 2) {
+        nameInfo.innerHTML = ""
+        nameInfo.innerHTML = "password length must be order 2 "
+        sumbitBtn.disabled = true
+        console.log('password length must be order 2')
+    } else {
+        nameInfo.innerHTML = ""
+        sumbitBtn.disabled = false
+    }
+})
+
+
+reg_email.addEventListener("change", () => {
+    if (reg_email.value.length < 5) {
+        emailInfo.innerHTML = ""
+        emailInfo.innerHTML = "password length must be order 5 "
+        sumbitBtn.disabled = true
+        console.log('password length must be order 5')
+    } else {
+        emailInfo.innerHTML = ""
+        sumbitBtn.disabled = false
+    }
+})
+
+
+reg_password.addEventListener("input", () => {
     // console.log(reg_password.length)
     if (reg_password.value.length < 8) {
         elem.innerHTML = ""
@@ -20,29 +49,31 @@ reg_password.addEventListener("change", () => {
     } else {
         elem.innerHTML = ""
         sumbitBtn.disabled = false
+        if (reg_password.value.length >= 8) {
+            elem.innerHTML = ""
+            sumbitBtn.disabled = true
+        }
+        if (reg_confirm_password.value) {
+            if (reg_password.value !== reg_confirm_password.value) {
+                elem.innerHTML = ""
+                elem1.innerHTML = "Confirm password do not match"
+                sumbitBtn.disabled = true
+                console.log('password length must be order 8')
+            }
+        }
     }
 })
 
 const elem1 = document.querySelector('.msg1')
 
-reg_confirm_password.addEventListener("change", () => {
-    if (reg_confirm_password.value.length < 8) {
+reg_confirm_password.addEventListener("input", () => {
+    if (reg_password.value !== reg_confirm_password.value) {
         elem1.innerHTML = ""
-        elem1.innerHTML = "password length must be order 8"
-        console.log('password length must be order 8')
+        elem1.innerHTML = "Confirm password do not match"
         sumbitBtn.disabled = true
-
     } else {
         elem1.innerHTML = ""
         sumbitBtn.disabled = false
-        if (reg_password.value !== reg_confirm_password.value) {
-            elem1.innerHTML = ""
-            elem1.innerHTML = "Confirm password do not match"
-            sumbitBtn.disabled = true
-        } else {
-            elem1.innerHTML = ""
-            sumbitBtn.disabled = false
-        }
     }
 })
 
@@ -63,5 +94,4 @@ function sendRequest() {
     })
         .then(res => res.json())
         .then(res => console.log(res))
-        .catch(error => console.error(error))
 }
